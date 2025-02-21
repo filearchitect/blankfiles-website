4f0281e5a0803d3281105705dd21cfc7867def62
Sébastien Lavoie <seblavoiehimself@gmail.com>
2025-02-20 23:28:51 -0500

    Update FileService.php

--- app/Services/FileService.php
+++ app/Services/FileService.php
@@ -14,14 +14,14 @@ class FileService {
     public function getAllFiles(): array {
         if (config('cache.cache_enabled')) {
             return Cache::remember('cdn_files', now()->addHours(1), function () {
-                return $this->fetchFilesFromCDN('/files/index.json');
+                return $this->fetchFilesFromCDN('/files/files.json');
             });
         }
 
-        return $this->fetchFilesFromCDN('/files/index.json');
+        return $this->fetchFilesFromCDN('/files/files.json');
     }
 
-    public function fetchFilesFromCDN(string $path = '/files/index.json') {
+    public function fetchFilesFromCDN(string $path = '/files/files.json') {
         return Http::get(config('app.cdn_url') . $path)->json();
     }
 
